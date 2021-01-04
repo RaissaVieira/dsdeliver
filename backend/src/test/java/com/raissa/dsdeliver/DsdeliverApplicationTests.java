fff1c77701298f36c9d66163091b160e2916a63a
Raissa Vieira <raissavieira125@gmail.com>
2021-01-04 09:31:56 -0300

    Project created

---
+++ backend/src/test/java/com/raissa/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raissa.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
