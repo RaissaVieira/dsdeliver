bd4778d51b2ca13173c10319fbe30b1d3bcfcbe1
Raissa Vieira <raissavieira125@gmail.com>
2021-01-07 11:40:02 -0300

    Add home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import NavBar from './Components/NavBar';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div>
+    <>
       <NavBar />
-    </div>
+      <Home />
+    </>
   );
 }
 
